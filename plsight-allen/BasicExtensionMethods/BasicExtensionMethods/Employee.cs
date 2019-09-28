ddc483b8da4f6d7cf160ae194e62fce728221509
klcantrell <cantrellkalalau@gmail.com>
2019-09-28 13:50:34 -0400

    plsight-allen: basic extension methods

---
+++ plsight-allen/BasicExtensionMethods/BasicExtensionMethods/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace BasicExtensionMethods
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ plsight-allen/BasicExtensionMethods/BasicExtensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+//using BasicExtensionMethods.Linq;
+
+namespace BasicExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] developers = new Employee[]
+            {
+                new Employee{ Id = 1, Name = "Scott" },
+                new Employee{ Id = 2, Name = "Chris" },
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee {  Id = 3, Name = "Alex" }
+            };
+
+            Console.WriteLine(sales.Count());
+        }
+    }
+}
