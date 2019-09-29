0e91817ecdb0a0bf04bbdcb85aa955c0b272fc75
klcantrell <cantrellkalalau@gmail.com>
2019-09-28 20:11:29 -0400

    plsight-allen: intro to lambda, func, action, and basic linq

--- plsight-allen/BasicExtensionMethods/BasicExtensionMethods/Program.cs
+++ plsight-allen/BasicExtensionMethods/BasicExtensionMethods/Program.cs
@@ -21,6 +21,18 @@ namespace BasicExtensionMethods
             };
 
             Console.WriteLine(sales.Count());
+
+            foreach (var employee in developers.Where(e => e.Name.Length == 5)
+                                               .OrderByDescending(e => e.Name))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) => x + y;
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square(add(3, 5)));
         }
     }
 }
