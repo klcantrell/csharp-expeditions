737d45640f92fff8d87b06867fec1b9512fba6f7
klcantrell <cantrellkalalau@gmail.com>
2019-09-28 21:01:36 -0400

    plsight-allen: implicit typing and Linq query syntax

--- plsight-allen/BasicExtensionMethods/BasicExtensionMethods/Program.cs
+++ plsight-allen/BasicExtensionMethods/BasicExtensionMethods/Program.cs
@@ -9,21 +9,28 @@ namespace BasicExtensionMethods
     {
         static void Main(string[] args)
         {
-            Employee[] developers = new Employee[]
+            var developers = new Employee[]
             {
                 new Employee{ Id = 1, Name = "Scott" },
                 new Employee{ Id = 2, Name = "Chris" },
             };
 
-            List<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
                 new Employee {  Id = 3, Name = "Alex" }
             };
 
             Console.WriteLine(sales.Count());
 
-            foreach (var employee in developers.Where(e => e.Name.Length == 5)
-                                               .OrderByDescending(e => e.Name))
+            //var query = developers.Where(e => e.Name.Length == 5)
+            //                                   .OrderByDescending(e => e.Name);
+
+            var query = from developer in developers
+                        where developer.Name.Length == 5
+                        orderby developer.Name
+                        select developer;
+
+            foreach (var employee in query)
             {
                 Console.WriteLine(employee.Name);
             }
