f70a41c5cd827c5fbf37ce18af4ab545a29226b4
klcantrell <cantrellkalalau@gmail.com>
2019-09-28 21:33:07 -0400

    plsight-allen: about deferred execution

---
+++ plsight-allen/Queries/Queries/MyLinq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queries
+{
+    public static class MyLinq
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    // defer execution until caller pulls value out
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ plsight-allen/Queries/Queries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Title = "The Dark Knight", Rating = 8.9f, Year =  2008 },
+                new Movie { Title = "The King's Speech'", Rating = 8.0f, Year =  2010 },
+                new Movie { Title = "Casablanca", Rating = 8.5f, Year =  1942 },
+                new Movie { Title = "Star Wars V", Rating = 8.7f, Year =  1980 }
+            };
+
+            //var query = movies.Where(m => m.Year > 2000);
+
+            // force execution instead of deferring because .Count() will 
+            // trigger execution and so would the loop for a second time
+            var query = movies.Filter(m => m.Year > 2000).ToList();
+
+            Console.WriteLine(query.Count());
+
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
+        }
+    }
+}
