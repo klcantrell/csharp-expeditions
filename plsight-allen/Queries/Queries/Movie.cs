67f90902785350333cb27d1c4605415f241d6574
klcantrell <cantrellkalalau@gmail.com>
2019-09-29 13:30:42 -0400

    plsight-allen: deferred and streaming execution

--- plsight-allen/Queries/Queries/Movie.cs
+++ plsight-allen/Queries/Queries/Movie.cs
@@ -6,8 +6,22 @@ namespace Queries
 {
     class Movie
     {
+        private float rating;
+        private int year;
+
         public string Title { get; set; }
         public float Rating { get; set; }
-        public int Year { get; set; }
+        public int Year 
+        { 
+            get
+            {
+                Console.WriteLine($"Returning {year} for {Title}");
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
     }
 }
--- plsight-allen/Queries/Queries/Program.cs
+++ plsight-allen/Queries/Queries/Program.cs
@@ -8,10 +8,16 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+            var numbers = MyLinq.Random().Where(n => n > 0.5).Take(10);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
             var movies = new List<Movie>
             {
                 new Movie { Title = "The Dark Knight", Rating = 8.9f, Year =  2008 },
-                new Movie { Title = "The King's Speech'", Rating = 8.0f, Year =  2010 },
+                new Movie { Title = "The King's Speech", Rating = 8.0f, Year =  2010 },
                 new Movie { Title = "Casablanca", Rating = 8.5f, Year =  1942 },
                 new Movie { Title = "Star Wars V", Rating = 8.7f, Year =  1980 }
             };
@@ -20,9 +26,15 @@ namespace Queries
 
             // force execution instead of deferring because .Count() will 
             // trigger execution and so would the loop for a second time
-            var query = movies.Filter(m => m.Year > 2000).ToList();
+            //var query = movies.Where(m => m.Year > 2000)
+            //                  .OrderBy(m => m.Rating);
 
-            Console.WriteLine(query.Count());
+            //Console.WriteLine(query.Count());
+
+            var query = from movie in movies
+                        where movie.Year > 2000
+                        orderby movie.Rating descending
+                        select movie;
 
             foreach (var movie in query)
             {
