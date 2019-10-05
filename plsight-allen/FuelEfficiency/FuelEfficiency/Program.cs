fe18c57ab1c88e586f34bcc39fed9a3938ea2667
klcantrell <cantrellkalalau@gmail.com>
2019-10-04 21:33:45 -0400

    plsight-allen: mapping with select and flat mapping with selectmany

--- plsight-allen/FuelEfficiency/FuelEfficiency/Program.cs
+++ plsight-allen/FuelEfficiency/FuelEfficiency/Program.cs
@@ -20,19 +20,54 @@ namespace FuelEfficiency
                 orderby car.Combined descending, car.Name
                 select car;
 
-            foreach (var car in query.Take(10))
+            var result = cars.SelectMany(c => c.Name)
+                             .OrderBy(c => c);
+
+            foreach (var character in result)
             {
-                Console.WriteLine($"{car.Name} : {car.City}");
+                Console.WriteLine(character);
             }
+
+            //foreach (var car in query.Take(10))
+            //{
+            //    Console.WriteLine($"{car.Name} : {car.City}");
+            //}
         }
 
         private static List<Car> ProcessFile(string path)
         {
             var query =
-                from line in File.ReadAllLines(path).Skip(1)
-                where line.Length > 1
-                select Car.ParseFromCsv(line);
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    .ToCar();
+
+            //from line in File.ReadAllLines(path).Skip(1)
+            //where line.Length > 1
+            //select Car.ParseFromCsv(line);
             return query.ToList();
+        }
+    }
+    public static class CarExtenions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
         }
     }
 }
