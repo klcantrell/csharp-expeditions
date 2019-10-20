9424440858e8f1d482a215f8c246bc24f2319fbf
klcantrell <cantrellkalalau@gmail.com>
2019-10-20 12:13:29 -0400

    plsight-allen/DotnetCoreFundamentals: using data service to fetch restaurant by id

--- plsight-allen/DotnetCoreFundamentals/DotnetCoreFundamentals.Data/IRestaurantData.cs
+++ plsight-allen/DotnetCoreFundamentals/DotnetCoreFundamentals.Data/IRestaurantData.cs
@@ -9,6 +9,7 @@ namespace DotnetCoreFundamentals.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -23,6 +24,11 @@ namespace DotnetCoreFundamentals.Data
                 new Restaurant { Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian },
                 new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican }
             };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
         }
 
         public IEnumerable<Restaurant> GetRestaurantsByName(string name)
--- plsight-allen/DotnetCoreFundamentals/DotnetCoreFundamentals/Pages/Restaurants/Detail.cshtml.cs
+++ plsight-allen/DotnetCoreFundamentals/DotnetCoreFundamentals/Pages/Restaurants/Detail.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotnetCoreFundamentals.Data;
 using DotnetCoreFundamentalsCoreLib;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,11 +11,18 @@ namespace DotnetCoreFundamentals.Pages.Restaurants
 {
     public class DetailModel : PageModel
     {
+        private readonly IRestaurantData restaurantData;
+
         public Restaurant Restaurant { get; set; }
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
         public void OnGet(int restaurantId)
         {
-            Restaurant = new Restaurant();
-            Restaurant.Id = restaurantId;
+            Restaurant = restaurantData.GetById(restaurantId);
         }
     }
 }
