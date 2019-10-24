ab5f274e70ee5fe59fba74f45674871e4c0419e8
klcantrell <cantrellkalalau@gmail.com>
2019-10-23 20:17:16 -0400

    plsight-allen/DotnetCoreFundamentals: add entity framework and extend dbcontext

---
+++ plsight-allen/DotnetCoreFundamentals/DotnetCoreFundamentals.Data/DotnetCoreFundamentalsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DotnetCoreFundamentalsCoreLib;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetCoreFundamentals.Data
+{
+    public class DotnetCoreFundamentalsDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
