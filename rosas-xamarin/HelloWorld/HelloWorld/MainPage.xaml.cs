1a92f06877fdb144e034df2038f259c0718db436
klcantrell <cantrellkalalau@gmail.com>
2020-08-21 22:32:43 -0400

    rosas-xamarin/hello-world: finish hello world app

---
+++ rosas-xamarin/HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Pressed(object sender, EventArgs e)
+        {
+            var name = nameEntry.Text;
+            var greeting = $"Hello {name}!";
+            greetingLabel.Text = greeting;
+            nameEntry.Text = null;
+        }
+    }
+}
