08595a572dac694954552014447439a63f27a603
klcantrell <cantrellkalalau@gmail.com>
2020-08-22 14:15:14 -0400

    rosas/travel: start travel app

---
+++ rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecord
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginButton_Pressed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
