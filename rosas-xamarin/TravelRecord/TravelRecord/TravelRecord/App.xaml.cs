21675c0ffc4d9d115a71426889bd9fa5e2a2413c
klcantrell <cantrellkalalau@gmail.com>
2020-08-26 21:21:05 -0400

    rosas/travel: Add sqlite to project

--- rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/App.xaml.cs
+++ rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/App.xaml.cs
@@ -6,12 +6,24 @@ namespace TravelRecord
 {
     public partial class App : Application
     {
+        public static string DatabaseLocation = string.Empty;
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
         }
+
+        public App(string databaseLocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            DatabaseLocation = databaseLocation;
+        }
+
 
         protected override void OnStart()
         {
