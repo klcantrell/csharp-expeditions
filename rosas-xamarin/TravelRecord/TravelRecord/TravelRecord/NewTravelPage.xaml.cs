da8e7fb0760dc7b66426026fb07c65647bf985c0
klcantrell <cantrellkalalau@gmail.com>
2020-08-27 20:48:33 -0400

    rosas/travel: Save a post to SQLite DB

--- rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/NewTravelPage.xaml.cs
+++ rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/NewTravelPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using SQLite;
+using TravelRecord.Model;
 using Xamarin.Forms;
 
 namespace TravelRecord
@@ -10,6 +11,25 @@ namespace TravelRecord
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        void Save_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = experienceEntry.Text
+            };
+
+            experienceEntry.Text = null;
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            var rows = conn.Insert(post);
+            conn.Close();
+
+            if (rows > 0)
+                DisplayAlert("Success", "Experience successfully inserted", "Ok");
+            else
+                DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
         }
     }
 }
