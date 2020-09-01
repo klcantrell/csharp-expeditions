28fa760e03ffa4903a55d9e2c4aaca134dd184a5
klcantrell <cantrellkalalau@gmail.com>
2020-08-31 21:59:08 -0400

    rosas/travel: move map to user's current location

--- rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/MapPage.xaml.cs
+++ rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/MapPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Plugin.Geolocator;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -7,6 +8,8 @@ namespace TravelRecord
 {
     public partial class MapPage : ContentPage
     {
+        private bool hasLocationPermission = false;
+
         public MapPage()
         {
             InitializeComponent();
@@ -31,7 +34,10 @@ namespace TravelRecord
 
                 if (status == PermissionStatus.Granted)
                 {
+                    hasLocationPermission = true;
                     locationsMap.IsShowingUser = true;
+
+                    GetLocation();
                 }
                 else
                 {
@@ -42,6 +48,59 @@ namespace TravelRecord
             {
                 await DisplayAlert("Error", "Something went wrong requesting your location", "Ok");
             }
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+
+                locator.PositionChanged += Locator_PositionChanged;
+
+                await locator.StartListeningAsync(TimeSpan.Zero, 100);
+            }
+
+            GetLocation();
+        }
+
+        protected override async void OnDisappearing()
+        {
+            var locator = CrossGeolocator.Current;
+
+            await locator.StopListeningAsync();
+
+            locator.PositionChanged -= Locator_PositionChanged;
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            Location location = new Location()
+            {
+                Latitude = e.Position.Latitude,
+                Longitude = e.Position.Longitude,
+            };
+            MoveMap(location);
+        }
+
+        private async void GetLocation()
+        {
+            if (hasLocationPermission)
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                MoveMap(location);
+            }
+        }
+
+        private void MoveMap(Location location)
+        {
+            var center = new Xamarin.Forms.Maps.Position(location.Latitude, location.Longitude);
+            var mapSpan = new Xamarin.Forms.Maps.MapSpan(center, 1, 1);
+            locationsMap.MoveToRegion(mapSpan);
         }
     }
 }
