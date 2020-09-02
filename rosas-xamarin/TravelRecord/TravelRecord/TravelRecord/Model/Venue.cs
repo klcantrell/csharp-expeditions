5663b5a3acfc68dc22c61c6f45173325bc566414
klcantrell <cantrellkalalau@gmail.com>
2020-09-01 21:10:23 -0400

    rosas/travel: add URL for foursquare API

---
+++ rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/Model/Venue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TravelRecord.Helpers;
+
+namespace TravelRecord.Model
+{
+    public class Venue
+    {
+        public static string GenerateURL(double latitude, double longitude)
+        {
+            return String.Format(Constants.VENUE_SEARCH, latitude, latitude, Constants.CLIENT_ID, Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
