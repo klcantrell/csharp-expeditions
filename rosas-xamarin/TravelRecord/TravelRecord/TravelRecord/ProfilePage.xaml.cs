8a596c56b0cefb983e629c4a0744b3f7bb127473
klcantrell <cantrellkalalau@gmail.com>
2020-09-06 20:36:30 -0400

    rosas/travel: Add posts count to profile page

--- rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/ProfilePage.xaml.cs
+++ rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/ProfilePage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-
+using SQLite;
+using TravelRecord.Model;
 using Xamarin.Forms;
 
 namespace TravelRecord
@@ -10,6 +12,18 @@ namespace TravelRecord
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using(SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var posts = conn.Table<Post>().ToList();
+
+                postCountLabel.Text = posts.Count.ToString();
+            }
         }
     }
 }
