7db44ccff190cf6cf2841e9603f5521927598d72
klcantrell <cantrellkalalau@gmail.com>
2020-10-09 23:13:32 -0400

    rosas/travel: Add calls to synchronization functionality

---
+++ rosas-xamarin/TravelRecord/TravelRecord/TravelRecord/Helpers/AzureAppServiceHelper.cs
@@ -0,0 +1,45 @@
+﻿#define OFFLINE_SYNC_ENABLED
+
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+
+namespace TravelRecord.Helpers
+{
+    public class AzureAppServiceHelper
+    {
+        public static async Task SyncAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await App.MobileService.SyncContext.PushAsync();
+
+                await App.postsTable.PullAsync("userPosts", "");
+            }
+            catch (MobileServicePushFailedException e)
+            {
+                syncErrors = e.PushResult?.Errors;
+            }
+            catch {}
+
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        await error.CancelAndDiscardItemAsync();
+                    }    
+                }
+            }
+        }
+    }
+}
