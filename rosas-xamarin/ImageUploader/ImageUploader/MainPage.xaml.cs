85abc0caeee32d7521fed50c0434dab7fc70a888
klcantrell <cantrellkalalau@gmail.com>
2020-10-11 21:14:02 -0400

    rosas/image-uploader: initial commit

---
+++ rosas-xamarin/ImageUploader/ImageUploader/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace ImageUploader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SelectImageButton_Pressed(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Error", "This is not supported on your device", "Ok");
+                return;
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium,
+            };
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if (selectedImageFile == null)
+            {
+                await DisplayAlert("Error", "There was an error getting an image", "Ok");
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        }
+    }
+}
