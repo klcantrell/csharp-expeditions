630bed62a4c293e07da4496cf5382ee176e98ffb
klcantrell <cantrellkalalau@gmail.com>
2020-12-27 13:34:16 -0500

    TestDisableReflection: initial commit

---
+++ TestDisableReflection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestDisableReflection
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await SendRequest();
+        }
+
+        private static async Task SendRequest()
+        {
+            int randomId;
+            do
+            {
+                randomId = random.Next(1, 84);
+            } while (randomId == 17);
+
+            var response = await client.GetStringAsync($"https://swapi.dev/api/people/{randomId}/");
+            Console.WriteLine($"Your random Star Wars character of the day:");
+            Console.WriteLine(response);
+        }
+    }
+}
