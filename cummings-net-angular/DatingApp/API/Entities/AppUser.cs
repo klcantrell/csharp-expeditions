9bd1f95b2cee533fbe91ec6ed2f139d8613b57ff
Kal Cantrell <klcantrell@sep.com>
2023-01-28 21:21:39 -0500

    [cummings-net-angular/DatingApp]: Create DB migration and first endpoint that fetches from DB

--- cummings-net-angular/DatingApp/API/Entities/AppUser.cs
+++ cummings-net-angular/DatingApp/API/Entities/AppUser.cs
@@ -2,13 +2,7 @@ namespace API.Entities
 {
     public class AppUser
     {
-        public AppUser(int id, String userName) 
-        {
-            this.Id = id;
-            this.UserName = userName;
-        }
-
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
     }
 }
