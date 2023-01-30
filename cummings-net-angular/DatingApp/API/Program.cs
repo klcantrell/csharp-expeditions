d57f9aea2d6a25fb6c2bba13310cf7e50f88133d
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-01-29 21:25:49 -0500

    [cummings-net-angular/DatingApp]: make HTTP request for users in Angular

--- cummings-net-angular/DatingApp/API/Program.cs
+++ cummings-net-angular/DatingApp/API/Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
