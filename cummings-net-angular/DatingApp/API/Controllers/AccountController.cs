b649d0a171d96f98dff1cbd615eaaf205f119827
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-02-04 20:09:30 -0500

    [cummings-net-angular/DatingApp]: Add JWT authentication/authorization to backend

--- cummings-net-angular/DatingApp/API/Controllers/AccountController.cs
+++ cummings-net-angular/DatingApp/API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,14 +12,16 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
 
-        public AccountController(DataContext context)
+        public AccountController(DataContext context, ITokenService tokenService)
         {
             _context = context;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username))
             {
@@ -39,11 +42,15 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto()
+            {
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user),
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.Username.ToLower());
             if (user == null)
@@ -62,8 +69,12 @@ namespace API.Controllers
                     return Unauthorized("no user found with that email and password");
                 }
             }
-            
-            return user;
+
+            return new UserDto()
+            {
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user),
+            };
         }
 
         private async Task<bool> UserExists(string username)
---
+++ cummings-net-angular/DatingApp/API/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
