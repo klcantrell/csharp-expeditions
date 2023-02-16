7247c3641e00f3fef94d8eeb3421e01612c1bcac
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-02-15 21:41:39 -0500

    [Montemagno/BrushUpXamarin] Initial commit from VS template for Xamarin forms
    Following this playlist from James Montemagno:
    https://www.youtube.com/playlist?list=PLwOF5UVsZWUiHY1CkRVjYJ6dm0iCvAlfw

---
+++ BrushUpXamarin/BrushUpXamarin/BrushUpXamarin/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BrushUpXamarin.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BrushUpXamarin.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
+
