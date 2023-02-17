c3046d27d30a48f0e11fe44d98fd80a1ffc82a04
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-02-16 21:40:17 -0500

    [montemagno/BrushUpXamarin]: Add basic layout and click handler

--- BrushUpXamarin/BrushUpXamarin/BrushUpXamarin/Views/CoffeeEquipmentPage.xaml.cs
+++ BrushUpXamarin/BrushUpXamarin/BrushUpXamarin/Views/CoffeeEquipmentPage.xaml.cs
@@ -7,10 +7,17 @@ namespace BrushUpXamarin.Views
 {
     public partial class CoffeeEquipmentPage : ContentPage
     {
+        int count = 0;
+
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
         }
+
+        void ButtonClick_Clicked(object sender, EventArgs e)
+        {
+            count += 1;
+            LabelCount.Text = $"You clicked {count} time(s)";
+        }
     }
 }
-
