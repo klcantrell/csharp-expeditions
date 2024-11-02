6403a06f81d64b58b102ed17fa318bc8faa67c23
Kalalau Cantrell <cantrellkalalau@gmail.com>
2024-11-01 22:11:58 -0400

    [cummings/datingapp] restart dotnet/angular course

--- cummings-net-angular/DatingApp/API/DTOs/UserDto.cs
+++
@@ -1,8 +0,0 @@
-namespace API.DTOs
-{
-    public class UserDto
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string Token { get; set; } = string.Empty;
-    }
-}
